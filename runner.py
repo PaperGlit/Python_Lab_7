06e28fffb4214bc8ac73e6a4540a503485874a30
Glit <vpad.lazar.04@gmail.com>
2024-11-24 01:45:17 +0200

    Near-complete non-refactored code (missing history functionality).

--- runner.py
+++ runner.py
@@ -1,5 +1,7 @@
+import re
 import requests
 from tabulate import tabulate
+from GlobalVariables import *
 from collections import defaultdict
 
 
@@ -31,52 +33,125 @@ def print_result(result):
     else:
         print("Invalid input!")
 
-def get_keys():
-    response = requests.get("https://jsonplaceholder.org/posts/1")
-    result = response.json()
-    keys = result.keys()
-    return keys
+def get(link):
+    prompt_1 = input("1 - GET all\n"
+                     "2 - GET by ID\n"
+                     "Your choice: ")
+    if prompt_1 == "1":
+        response = requests.get(f"https://jsonplaceholder.typicode.com/{link}")
+        result = response.json()
+        print_result(result)
+    elif prompt_1 == "2":
+        prompt_2 = input("Enter ID: ")
+        response = requests.get(f"https://jsonplaceholder.typicode.com/{link}/{prompt_2}")
+        result = response.json()
+        print_result([result])
+    else:
+        print("Invalid input!")
+
+def form_request(link, data, is_post_request=True):
+    request = {}
+    for key, item in data.items():
+        if isinstance(item, dict):
+            request[key] = form_request(link, item, is_post_request)
+        elif key != "id":
+            prompt_1 = input("Enter " + key + ": ")
+            if prompt_1.strip() != "":
+                if "id" in key.lower():
+                    prompt_1 = int(prompt_1)
+                elif not (key.lower() == "email" and re.match(prompt_1, email_regex)):
+                    raise ValueError("The email value is incorrect!")
+                elif not ((key.lower() == "url" or key.lower() == "website") and re.match(prompt_1, link_regex)):
+                    raise ValueError("The website value is incorrect!")
+                elif not (key.lower() == "phone" and re.match(prompt_1, phone_regex)):
+                    raise ValueError("The phone value is incorrect!")
+                elif not (key.lower() == "lat" and (-90 <= int(prompt_1) <= 90)):
+                    raise ValueError("The latitude value is incorrect!")
+                elif not (key.lower() == "lng" and (-180 <= int(prompt_1) <= 180)):
+                    raise ValueError("The longitude value is incorrect!")
+                request[key] = prompt_1
+            else:
+                if is_post_request:
+                    raise ValueError("The value cannot be empty!")
+    return request
+
+def post(link):
+    response = requests.get(f"https://jsonplaceholder.typicode.com/{link}/1")
+    data = response.json()
+    try:
+        request = form_request(link, data)
+    except ValueError as e:
+        print(e)
+        return
+    response = requests.post(f"https://jsonplaceholder.typicode.com/{link}", request)
+    if response.ok:
+        print(f"The post with id {response.json().get('id')} has been added successfully.")
+    else:
+        print(f"An error occurred: {response.text}")
+
+def patch(link):
+    prompt_1 = input("Enter ID: ")
+    response = requests.get(f"https://jsonplaceholder.typicode.com/{link}/{prompt_1}")
+    data = response.json()
+    try:
+        request = form_request(link, data, False)
+    except ValueError as e:
+        print(e)
+        return
+    response = requests.patch(f"https://jsonplaceholder.typicode.com/{link}/{data["id"]}", request)
+    if response.ok:
+        print(f"The post's data with id {data["id"]} has been changed successfully.")
+    else:
+        print(f"An error occurred: {response.text}")
+
+def delete(link):
+    prompt_1 = input("Enter ID: ")
+    response = requests.delete(f"https://jsonplaceholder.typicode.com/{link}/{prompt_1}")
+    if response.ok:
+        print(f"The post with id {prompt_1} has been deleted successfully.")
+    else:
+        print(f"An error occurred: {response.text}")
 
 def main():
     while True:
-        prompt = input("\n1 - GET\n"
+        prompt = input("\n1 - Posts\n"
+                         "2 - Comments\n"
+                         "3 - Albums\n"
+                         "4 - Photos\n"
+                         "5 - Todos\n"
+                         "6 - Users\n"
+                         "Your choice: ")
+        match prompt:
+            case "1":
+                link = "posts"
+            case "2":
+                link = "comments"
+            case "3":
+                link = "albums"
+            case "4":
+                link = "photos"
+            case "5":
+                link = "todos"
+            case "6":
+                link = "users"
+            case _:
+                print("Invalid input!")
+                break
+
+        prompt_1 = input("1 - GET\n"
                          "2 - POST\n"
                          "3 - PATCH\n"
                          "4 - DELETE\n"
                          "Your choice: ")
-        match prompt:
+        match prompt_1:
             case "1":
-                prompt_1 = input("1 - GET all\n"
-                                 "2 - GET by ID\n"
-                                 "Your choice: ")
-                if prompt_1 == "1":
-                    response = requests.get("https://jsonplaceholder.org/posts")
-                    result = response.json()
-                    print_result(result)
-                elif prompt_1 == "2":
-                    post_id = input("Enter ID: ")
-                    response = requests.get("https://jsonplaceholder.org/posts/" + post_id)
-                    result = response.json()
-                    print_result([result])
-                else:
-                    print("Invalid input!")
+                get(link)
             case "2":
-                request = {}
-                keys = get_keys()
-                for key in keys:
-                    # if key == "url":
-                    #     request[key] = f"https://jsonplaceholder.org/posts/{request["slug"]}"
-                    if key != "id":
-                        prompt_2 = input("Enter " + key + ": ")
-                        if prompt_2.strip() != "":
-                            request[key] = prompt_2
-                        else:
-                            print("The value cannot be empty!")
-                            break
-                response = requests.post("https://jsonplaceholder.org/posts", request)
-                print(response.text)
+                post(link)
             case "3":
-                pass
+                patch(link)
+            case "4":
+                delete(link)
             case _:
                 break
 main()
