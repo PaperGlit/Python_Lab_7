a8dfb1dcdc137bd14bdaf9001f72a7428a65b0c8
Glit <vpad.lazar.04@gmail.com>
2024-11-24 03:31:06 +0200

    Finished work on the project

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,3 +1,13 @@
+base_api_url = "https://jsonplaceholder.typicode.com"
 link_regex = r'^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,6}(\/[^\s]*)?$'
 email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 phone_regex = r'^\+380(20|39|50|63|66|67|68|73|75|77|89|91|92|93|94|95|96|97|98|99)\d{7}$'
+header_style = "bold white on blue"
+entity_map = {
+    "1": "posts",
+    "2": "comments",
+    "3": "albums",
+    "4": "photos",
+    "5": "todos",
+    "6": "users",
+}
